679c1c63cfdd472511a1054c108fe3184cc8e582
migellebarlis <migellejosebarlis@gmail.com>
2022-12-14 23:45:54 +0800

    update loss function then add validation and test

--- demo.m
+++ demo.m
@@ -15,6 +15,16 @@ numImgs = numel(imds.Files);
 % Read all images from the image datastore to a cell array
 ims = reshape(readall(imds),[numImgs/numClasses numClasses]);
 
+% Remove the means from all the images
+for i = 1:numel(ims)
+    ims{i} = ims{i} - mean(ims{i}(:));
+end
+
+% Divide the images for training, validation, and testing
+train = ims(1:60,:);
+valid = ims(61:70,:);
+test = ims(71:80,:);
+
 % The network used in this example requires input images of size
 % 128-by-128.
 inputSize = [128 128 1];
@@ -31,74 +41,71 @@ layers = [
     convolution2dLayer(3,64, Name='feat_3') % 28 x 28
     reluLayer(Name='relu_3')
     maxPooling2dLayer(2,Stride=[2 2],Name='pool_3')]; % 14 x 14
-%     batchNormalizationLayer(Name='norm')
-%     fft2Layer(Name='fft2')];
-%     fullyConnectedLayer(numClasses, Name='full')
-%     softmaxLayer(Name='softmax')];
 
 % Create a dlnetwork object from the layer array.
-net = dlnetwork(layers)
+net = dlnetwork(layers);
 
-% Train for 300 epochs
-numEpochs = 300;
-batchSize = 20;
-
-% Specify the options for SGDM optimization. Specify an initial learn rate
-% of 0.01 with a decay of 0.01, and momentum 0.9.
-initialLearnRate = 0.01;
-decay = 0.01;
-momentum = 0.9;
+% Train for n epochs
+numEpochs = 100;
 
 % Initialize the training progress plot.
 figure
 C = colororder;
 lineLossTrain = animatedline(Color=C(2,:));
-ylim([0 inf])
 xlabel("Epoch")
 ylabel("Loss")
 grid on
 
-% Initialize the velocity parameter for the SGDM solver.
-velocity = [];
+% Initialize the gradient parameter for the ADAM solver.
+averageGrad = [];
+averageSqGrad = [];
 
 % Train the network using a custom training loop.
 start = tic;
 
 % Loop over epochs.
 for epoch = 1:numEpochs
-    X1 = zeros([inputSize batchSize]);
-    X2 = zeros([inputSize batchSize]);
-    Xsel = randsample(numImgs/numClasses,batchSize);
-
-    for i = 1:batchSize
-        Csel = randsample(numClasses,2);
-        X1(:,:,:,i) = im2double(ims{Xsel(i),Csel(1)});
-        X2(:,:,:,i) = im2double(ims{Xsel(i),Csel(2)});
-    end
+    % Preprocess the training data
+    X = preprocessTrainingData(train,inputSize);
 
     % Evaluate the model gradients, state, and loss using dlfeval and the
     % modelLoss function and update the network state.
-    [loss,gradients,state] = dlfeval(@modelLoss,net,X1,X2);
+    [gradients,state] = dlfeval(@modelLoss,net,X);
     net.State = state;
-    
-    % Determine learning rate for time-based decay learning rate schedule.
-    learnRate = initialLearnRate/(1 + decay*epoch);
-    
-    % Update the network parameters using the SGDM optimizer.
-    [net,velocity] = sgdmupdate(net,gradients,velocity,learnRate,momentum);
+
+    % Update the network parameters using the ADAM optimizer.
+    [net,averageGrad,averageSqGrad] = adamupdate(net,gradients,averageGrad,averageSqGrad,epoch);
+
+    % Preprocess the validation data
+    X = preprocessTrainingData(valid,inputSize);
+
+    % Validate the model by getting the loss
+    validLoss = modelValidate(net,X);
     
     % Display the training progress.
     D = duration(0,0,toc(start),Format="hh:mm:ss");
-    loss = double(loss);
-    addpoints(lineLossTrain,epoch,loss)
+    validLoss = double(validLoss);
+    addpoints(lineLossTrain,epoch,validLoss)
     title("Epoch: " + epoch + ", Elapsed: " + string(D))
     drawnow
 end
 
-function [loss,gradients,state] = modelLoss(net,X1,X2)
-% Concatenate model inputs
-X = dlarray(cat(4,X1,X2),"SSCB");
+% Preprocess the test data
+X = preprocessTestData(valid,inputSize);
 
+% Test the model
+testLoss = modelTest(net,X)';
+
+% Visualize the loss.
+comb = nchoosek(1:8,2);
+figure
+scatter(1:28,testLoss',"filled")
+xlabel("Kernel Combination Index")
+ylabel("Loss")
+title("Test Loss Summary")
+legend(strcat('Image',arrayfun(@num2str,(1:10)','UniformOutput',false)))
+
+function [gradients,state] = modelLoss(net,X)
 % Forward data through network.
 [Y,state] = forward(net,X);
 
@@ -106,16 +113,101 @@ numFeat = numel(Y(:,:,:,1));
 numBatch = size(Y,4)/2;
 
 Y = reshape(Y,[numFeat numBatch*2]);
-loss = dlarray(zeros([numBatch 1]));
+Y1 = Y(:,1:numBatch);
+Y2 = Y(:,numBatch+1:end);
+
+% Create penalties for feature vectors approaching zero
+lambda = 1;
+p1 = lambda * exp(-(sum(Y1 .* Y1) .^ 2) ./ 0.01);
+p2 = lambda * exp(-(sum(Y2 .* Y2) .^ 2) ./ 0.01);
+p = sum(p1 + p2);
 
 % Calculate normalised dot product.
-for i = 1:numBatch
-    loss(i) = Y(:,i)' * Y(:,numBatch+i) ./ sqrt(Y(:,i)' * Y(:,i)) ./ sqrt(Y(:,numBatch+i)' * Y(:,numBatch+i));
-end
-
-loss = sum(loss.*loss);
+loss = sum((sum(Y1 .* Y2) .^ 2) ./ (sum(Y1 .* Y1 + Y2 .* Y2) + eps)) + p;
 
 % Calculate gradients of loss with respect to learnable parameters.
 gradients = dlgradient(loss,net.Learnables);
 
+end
+
+function loss = modelValidate(net,X)
+% Forward data through network.
+Y = predict(net,X);
+
+numFeat = numel(Y(:,:,:,1));
+numBatch = size(Y,4)/2;
+
+Y = reshape(Y,[numFeat numBatch*2]);
+Y1 = Y(:,1:numBatch);
+Y2 = Y(:,numBatch+1:end);
+
+% Create penalties for feature vectors approaching zero
+lambda = 1;
+p1 = lambda * exp(-(sum(Y1 .* Y1) .^ 2) ./ 0.01);
+p2 = lambda * exp(-(sum(Y2 .* Y2) .^ 2) ./ 0.01);
+p = sum(p1 + p2);
+
+% Calculate normalised dot product.
+loss = sum((sum(Y1 .* Y2) .^ 2) ./ (sum(Y1 .* Y1 + Y2 .* Y2) + eps)) + p;
+
+end
+
+function loss = modelTest(net,X)
+% Make prediction.
+Y = predict(net,X);
+
+numFeat = numel(Y(:,:,:,1));
+numBatch = size(Y,4)/8;
+
+Y = reshape(Y,[numFeat numBatch 8]);
+loss = zeros([numBatch 8]);
+comb = nchoosek(1:8,2);
+
+for i = 1:size(comb,1)
+    Y1 = squeeze(Y(:,:,comb(i,1)));
+    Y2 = squeeze(Y(:,:,comb(i,2)));
+
+    % Create penalties for feature vectors approaching zero
+    lambda = 1;
+    p1 = lambda * exp(-(sum(Y1 .* Y1) .^ 2) ./ 0.01);
+    p2 = lambda * exp(-(sum(Y2 .* Y2) .^ 2) ./ 0.01);
+    p = p1 + p2;
+    
+    % Calculate normalised dot product.
+    loss(:,i) = (((sum(Y1 .* Y2) .^ 2) ./ (sum(Y1 .* Y1 + Y2 .* Y2) + eps)) + p)';
+end
+
+end
+
+function X = preprocessTrainingData(I,inputSize)
+numBatch = size(I,1);
+numClass = size(I,2);
+
+X1 = zeros([inputSize numBatch]);
+X2 = zeros([inputSize numBatch]);
+Xsel = randperm(numBatch);
+
+for i = 1:numBatch
+    Csel = randperm(numClass,2);
+    X1(:,:,:,i) = im2double(I{Xsel(i),Csel(1)});
+    X2(:,:,:,i) = im2double(I{Xsel(i),Csel(2)});
+end
+
+X = dlarray(cat(4,X1,X2),"SSCB");
+
+end
+
+function X = preprocessTestData(I,inputSize)
+numBatch = size(I,1);
+numClass = size(I,2);
+
+X = zeros([inputSize numBatch*numClass]);
+
+for i = 1:numClass
+    for j = 1:numBatch
+        X(:,:,:,j+(i-1)*numBatch) = im2double(I{j,i});
+    end
+end
+
+X = dlarray(X,"SSCB");
 end
